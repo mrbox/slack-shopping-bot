9bd3dfd45e6ed059a266d7c7c5d65f1958ca1351
Jakub Paczkowski <jakub@paczkowski.eu>
2016-02-02 15:59:22 -0800

    redis

--- shopping_list.py
+++ shopping_list.py
@@ -1,7 +1,26 @@
+import os
+
+import redis
 from slackbot.bot import respond_to
+
+r = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], db=0, password=os.environ['REDIS_PASS'])
+list_name = 'shopping_list'
+
 
 @respond_to('show list')
 def show_list(message):
-    message.reply("Show list")
+    message.reply("There is %s items on the list:" % r.llen(list_name))
+    for obj in r.get(list_name):
+        message.reply("* %s" % obj)
 
 
+@respond_to('add to list (.*)')
+def add_to_list(message, item):
+    r.rpush(list_name, item)
+    message.reply("You've added %s to list" % item)
+
+
+@respond_to('clear list')
+def show_list(message):
+    r.delete(list_name)
+    message.reply("You've cleared the shopping list")
