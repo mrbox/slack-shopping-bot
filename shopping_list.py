06a860200c90f40be26cb61e37c1ecc1a689a90f
Jakub Paczkowski <jakub@paczkowski.eu>
2016-02-03 21:45:40 -0800

    capitals

--- shopping_list.py
+++ shopping_list.py
@@ -69,7 +69,7 @@ def show_list(message, *args):
 def add_to_list(message, *args):
     item=args[-1]
     user = message._client.users[message._body['user']]
-    r.rpush(list_name, "*%s* (by %s)" % (str(item), user['name']))
+    r.rpush(list_name, "*%s* (by %s)" % (str(item).title(), user['name']))
     message.reply("You've added %s to list" % item)
     message.send("_%s_" % random.choice(catchall_responses))
 
